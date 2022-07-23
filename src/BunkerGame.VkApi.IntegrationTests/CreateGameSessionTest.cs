7bdb3e3628d25e155f8b4107b14eed2a7292c77b
kirillf2412 <kirillf2412@gmail.com>
2022-07-23 11:50:51 +0300

    Restat gameSession test was added

--- src/BunkerGame.VkApi.IntegrationTests/CreateGameSessionTest.cs
+++ src/BunkerGame.VkApi.IntegrationTests/CreateGameSessionTest.cs
@@ -15,7 +15,7 @@ namespace BunkerGame.VkApi.IntegrationTests
     {
         private const string commandText = "Бот,создать новую игру";
         [Fact]
-        public async void CreateGameCommand_NewGameSession()
+        public async void CreateGameCommand_GameNotCreated_NewGameSession()
         {
             var peerId = new Random().NextInt64(2000000000, 10000000000);
             var bag = MessageBagFactory.CreateMessageBug(peerId, 6);
@@ -31,6 +31,27 @@ namespace BunkerGame.VkApi.IntegrationTests
             Assert.True(conversation.Users.Count == 6);
             Assert.True(gameSession.Name == conversation.ConversationName);
             Assert.True(gameSession.GameState == GameState.Preparation);
+        }
+        [Fact]
+        public async void CreateGameCommand_GameCreated_GameSessionRestarted()
+        {
+            var peerId = new Random().NextInt64(2000000000, 10000000000);
+            var bag = MessageBagFactory.CreateMessageBug(peerId, 5);
+            var bagConversation = bag.Conversations[peerId];
+            var userSender = bagConversation.Users.First().Id;
+            using var services = ServiceBuilder.GetServiceProvider(bag);
+            var messageService = services.GetService<IMessageService>()!;
+
+            await messageService.SendStartGameCommand(userSender, peerId);
+            await messageService.SendCreateGameSessionCommand(userSender, peerId);
+            
+            var conversation = await services.GetService<IConversationRepository>()!.GetConversation(peerId);
+            var gameSession = await services.GetService<IGameSessionRepository>()!.GetGameSession(conversation!.GameSessionId);
+            Assert.True(gameSession.GameState == GameState.Preparation);
+            Assert.Empty(gameSession.ExternalSurroundings);
+            Assert.True(gameSession.Catastrophe == Catastrophe.DefaultCatastrophe);
+            Assert.Empty(gameSession.Characters);
+
         }
     }
 }
