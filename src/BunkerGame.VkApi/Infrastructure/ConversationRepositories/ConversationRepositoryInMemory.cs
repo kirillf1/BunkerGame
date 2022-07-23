56393463778e0f384cf64a67938017e97c303e20
kirillf2412 <kirillf2412@gmail.com>
2022-07-23 11:54:19 +0300

    Changed private collections to System.Collections.Concurrent

--- src/BunkerGame.VkApi/Infrastructure/ConversationRepositories/ConversationRepositoryInMemory.cs
+++ src/BunkerGame.VkApi/Infrastructure/ConversationRepositories/ConversationRepositoryInMemory.cs
@@ -1,68 +1,63 @@
 ﻿using BunkerGame.Domain.Shared;
 using BunkerGame.VkApi.VkGame;
+using System.Collections.Concurrent;
 
 namespace BunkerGame.VkApi.Infrastructure.ConversationRepositories
 {
     public class ConversationRepositoryInMemory : IConversationRepository
     {
-        private List<Conversation> _conversations;
+        private ConcurrentDictionary<long, Conversation> _conversations;
+        object syncObj = new object();
         public ConversationRepositoryInMemory()
         {
-            _conversations = new List<Conversation>();
+            _conversations = new ConcurrentDictionary<long, Conversation>();
+
         }
         public Task AddConversation(Conversation conversation)
         {
-            if (_conversations.Any(c => c.ConversationId == conversation.ConversationId))
-            {
-                UpdateConversation(conversation);
-                return Task.CompletedTask;
-            }
-            _conversations.Add(conversation);
+            _conversations.TryAdd(conversation.ConversationId, conversation);
             return Task.CompletedTask;
         }
 
         public Task DeleteConversation(long id)
         {
-            var conversation = _conversations.Find(c => c.ConversationId == id);
-            if (conversation != null)
-                _conversations.Remove(conversation);
+            _conversations.TryRemove(id, out _);
             return Task.CompletedTask;
         }
 
         public Task<Conversation?> GetConversation(string name)
         {
-            return Task.Run(() => _conversations.Find(c => c.ConversationName.Contains(name, StringComparison.OrdinalIgnoreCase)));
+            return Task.Run(() => _conversations.Select(c => c.Value).FirstOrDefault(c => c.ConversationName.Contains(name, StringComparison.OrdinalIgnoreCase)));
         }
 
         public Task<Conversation?> GetConversation(long id)
         {
-            return Task.Run(() => _conversations.Find(c => c.ConversationId == id));
+            return Task.Run(() =>
+            {
+                if (_conversations.TryGetValue(id, out var conversation))
+                    return conversation;
+                return null;
+            });
         }
 
         public Task<Conversation?> GetConversation(GameSessionId gameSessionId)
         {
-            return Task.Run(() => _conversations.Find(c => c.GameSessionId == gameSessionId));
+            return Task.Run(() => _conversations.Select(c => c.Value).FirstOrDefault(c => c.GameSessionId == gameSessionId));
         }
 
         public Task<Conversation?> GetConversationByCharacterId(CharacterId characterId)
         {
-            return Task.Run(() => _conversations.Find(c => c.Users.Any(c => c.CharacterId == characterId)));
+            return Task.Run(() => _conversations.Select(c => c.Value).FirstOrDefault(c => c.Users.Any(c => c.CharacterId == characterId)));
         }
 
         public Task<IEnumerable<Conversation>> GetConversationsByUserId(long userId)
         {
-            return Task.Run(() => _conversations.Where(c => c.Users.Any(c => c.UserId == userId)));
+            return Task.Run(() => _conversations.Select(c => c.Value).Where(c => c.Users.Any(c => c.UserId == userId)));
         }
 
         public Task UpdateConversation(Conversation conversation)
         {
-            var conversationUpdate = _conversations.Find(c => c.ConversationId == conversation.ConversationId);
-            if (conversationUpdate != null)
-            {
-                conversationUpdate = conversation;
-            }
             return Task.CompletedTask;
-
         }
     }
 }
--- src/BunkerGame.VkApi/Infrastructure/PlayersRepository/PlayersRepositoryChache.cs
+++ src/BunkerGame.VkApi/Infrastructure/PlayersRepository/PlayersRepositoryChache.cs
@@ -1,6 +1,7 @@
 ﻿using BunkerGame.Domain.Players;
 using BunkerGame.Domain.Shared;
 using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Concurrent;
 using System.Linq.Expressions;
 
 namespace BunkerGame.VkApi.Infrastructure.PlayersRepository
@@ -15,16 +16,16 @@ namespace BunkerGame.VkApi.Infrastructure.PlayersRepository
         }
         public Task AddPlayer(Player player)
         {
-            List<Player> players;
+            ConcurrentBag<Player> players;
             if (memoryChache.TryGetValue(_PlayersKey, out players))
             {
-                var oldPlayer = players.Find(c => c.Id == player.Id);
+                var oldPlayer = players.FirstOrDefault(c => c.Id == player.Id);
                 if (oldPlayer != null)
                     return Task.CompletedTask;
                 players.Add(player);
                 return Task.CompletedTask;
             }
-            players = new List<Player>();
+            players = new ConcurrentBag<Player>();
             players.Add(player);
             memoryChache.Set(_PlayersKey, players);
             return Task.CompletedTask;
@@ -34,9 +35,9 @@ namespace BunkerGame.VkApi.Infrastructure.PlayersRepository
         {
             return await Task.Run(() =>
             {
-                if (memoryChache.TryGetValue(_PlayersKey, out List<Player> players))
+                if (memoryChache.TryGetValue(_PlayersKey, out ConcurrentBag<Player> players))
                 {
-                    var player = players.Find(c => c.Id == playerId);
+                    var player = players.FirstOrDefault(c => c.Id == playerId);
                     if (player != null)
                         return player;
                 }
@@ -48,9 +49,9 @@ namespace BunkerGame.VkApi.Infrastructure.PlayersRepository
         {
             return await Task.Run(() =>
             {
-                if (memoryChache.TryGetValue(_PlayersKey, out List<Player> players))
+                if (memoryChache.TryGetValue(_PlayersKey, out ConcurrentBag<Player> players))
                 {
-                    var player = players.Find(c => c.FirstName == firstName && c.LastName == lastName);
+                    var player = players.FirstOrDefault(c => c.FirstName == firstName && c.LastName == lastName);
                     if (player != null)
                         return player;
                 }
@@ -62,7 +63,7 @@ namespace BunkerGame.VkApi.Infrastructure.PlayersRepository
         {
             return await Task.Run(() =>
             {
-                if (memoryChache.TryGetValue(_PlayersKey, out List<Player> players))
+                if (memoryChache.TryGetValue(_PlayersKey, out ConcurrentBag<Player> players))
                 {
                     var query = players.AsQueryable();
                     if (predicate != null)
@@ -77,9 +78,9 @@ namespace BunkerGame.VkApi.Infrastructure.PlayersRepository
         {
             return await Task.Run(() =>
             {
-                if (memoryChache.TryGetValue(_PlayersKey, out List<Player> players))
+                if (memoryChache.TryGetValue(_PlayersKey, out ConcurrentBag<Player> players))
                 {
-                    var player = players.Find(c => c.FirstName == firstName && c.LastName == lastName);
+                    var player = players.FirstOrDefault(c => c.FirstName == firstName && c.LastName == lastName);
                     if (player != null)
                         return true;
                     return false;
@@ -92,9 +93,9 @@ namespace BunkerGame.VkApi.Infrastructure.PlayersRepository
         {
             await Task.Run(() =>
            {
-               if (memoryChache.TryGetValue(_PlayersKey, out List<Player> players))
+               if (memoryChache.TryGetValue(_PlayersKey,out ConcurrentBag<Player> players))
                {
-                   players.Remove(player);
+                   players.TryTake(out player);
                }
            });
         }
