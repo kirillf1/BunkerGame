334cdef083f74eb490da186ac10dbcfc11369ec7
kirillf2412 <kirillf2412@gmail.com>
2022-07-29 21:31:30 +0300

    Added action services

---
+++ src/BunkerGame.VkApi/VkGame/VkGameServices/ActionServices/InvitedInConversationService.cs
@@ -0,0 +1,27 @@
+﻿using VkNet.Abstractions;
+
+namespace BunkerGame.VkApi.VkGame.VkGameServices.ActionServices
+{
+    public class InvitedInConversationService
+    {
+        private readonly IVkApi vkApi;
+        private readonly ILogger<InvitedInConversationService> logger;
+        private readonly IConversationRepository conversationRepository;
+
+        public InvitedInConversationService(IVkApi vkApi, ILogger<InvitedInConversationService> logger, IConversationRepository conversationRepository)
+        {
+            this.vkApi = vkApi;
+            this.logger = logger;
+            this.conversationRepository = conversationRepository;
+        }
+        public async Task SendGreeting(long peerId)
+        {
+            var conversation = await conversationRepository.GetConversation(peerId);
+            if (conversation != null)
+                return;
+            await vkApi.SendVKMessage("Привет, я бот для игры в бункер. Отправляю свои команды. Также дайте права администратора в беседе, чтоб я мог создать игру",peerId, VkKeyboardFactory.CreateStartGameButtons());
+            logger.LogInformation("Bot added in conversation with id {id}", peerId);
+            
+        }
+    }
+}
