edfc0e3f7d5167d42707142a4e462de23d17abdf
kirillf2412 <kirillf2412@gmail.com>
2022-08-04 16:24:46 +0300

    Init BunkerGameComponents.Editor

---
+++ src/BunkerGameComponetns.Editor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BunkerGameComponetns.Editor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/BunkerGameComponetns.Editor/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BunkerGameComponetns.Editor
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }     
+    }
+}
