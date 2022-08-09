24c65a720ff6b6092ee325f4c48523a4f4d6ce05
kirillf2412 <kirillf2412@gmail.com>
2022-08-09 14:17:35 +0300

    UnitOfWork added

---
+++ src/BunkerGameComponents.Infrastructure/UnitOfWork/UnitOfWorkJson.cs
@@ -0,0 +1,20 @@
+﻿using BunkerGameComponents.Domain;
+using BunkerGameComponents.Infrastructure.Database.GameComponentContext;
+
+namespace BunkerGameComponents.Infrastructure.UnitOfWork
+{
+    public class UnitOfWorkJson : IUnitOfWork
+    {
+        private readonly GameComponentJsonContext context;
+
+        public UnitOfWorkJson(GameComponentJsonContext context)
+        {
+            this.context = context;
+        }
+        public Task Save(CancellationToken cancellationToken)
+        {
+            context.SaveChanges();
+            return Task.CompletedTask;
+        }
+    }
+}
